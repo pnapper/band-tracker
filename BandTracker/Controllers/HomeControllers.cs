1f11faf42f8fba13be69941d2a697ad3ed8d9e3b
Patrick Napper <patrick@patricknapper.com>
2017-11-03 12:14:37 -0700

    continues adding routes to HomeControllers.

--- BandTracker/Controllers/HomeControllers.cs
+++ BandTracker/Controllers/HomeControllers.cs
@@ -16,7 +16,7 @@ namespace BandTracker.Controllers
     [HttpGet("/band")]
     public ActionResult Bands()
     {
-      List<Student> allBands = Band.GetAll();
+      List<Band> allBands = Band.GetAll();
       return View(allBands);
     }
     [HttpGet("/venue")]
@@ -24,6 +24,36 @@ namespace BandTracker.Controllers
     {
       List<Venue> allVenues = Venue.GetAll();
       return View(allVenues);
+    }
+
+    //NEW BAND
+    [HttpGet("/bands/new")]
+    public ActionResult BandForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/bands/new")]
+    public ActionResult BandCreate()
+    {
+      Band newBand = new Band(Request.Form["band-name"]);
+      newBand.Save();
+      return View("Success");
+    }
+
+    //NEW VENUE
+    [HttpGet("/venues/new")]
+    public ActionResult VenueForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/venues/new")]
+    public ActionResult VenueCreate()
+    {
+      Venue newVenue = new Venue(Request.Form["venue-name"]);
+      newVenue.Save();
+      return View("Success");
     }
   }
 }
